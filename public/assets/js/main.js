3a4dd9be3788ec1b4b443a90865f217627b98ef1
viniciusmweb <vinicius.silva1076@etec.sp.gov.br>
2022-01-23 12:57:48 -0300

    ajustes

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,8 +1,2 @@
 "use strict";
-var soma;
-console.log('funcionando');
-var add = function (a, b, c) {
-    if (c === void 0) { c = 10; }
-    console.log(a + b);
-    console.log(c);
-};
+// tsc -w
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,1 @@
-let soma: Function;
-
-console.log('funcionando');
-
-const add = (a: number, b: number, c: number|string = 10) => {
-  console.log(a+b);
-  console.log(c);
-}
+// tsc -w
