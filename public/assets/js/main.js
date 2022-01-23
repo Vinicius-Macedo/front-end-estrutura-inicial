603f7b0b94a09863174a2e1e10af5959d068ad21
viniciusmweb <vinicius.silva1076@etec.sp.gov.br>
2022-01-23 12:56:35 -0300

    simples ajuste

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,2 +1,8 @@
 "use strict";
-console.log('tesstsesdsdsssssssssss');
+var soma;
+console.log('funcionando');
+var add = function (a, b, c) {
+    if (c === void 0) { c = 10; }
+    console.log(a + b);
+    console.log(c);
+};
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,8 @@
-// tsc -w
-console.log('Hellow World');
+let soma: Function;
+
+console.log('funcionando');
+
+const add = (a: number, b: number, c: number|string = 10) => {
+  console.log(a+b);
+  console.log(c);
+}
